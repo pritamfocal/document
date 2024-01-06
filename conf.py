59de53b8384a3a1e9ca0f9969e96abe6e6ac5784
pritam bolenwar <pritam.bolenwar53@gmail.com>
2024-01-06 19:08:20 +0100

    adding first changes

---
+++ conf.py
@@ -0,0 +1,13 @@
+# conf.py
+
+# Add Read the Docs Theme
+import sphinx_rtd_theme
+
+extensions = [
+    # ... other extensions
+    "sphinx_rtd_theme",
+]
+
+html_theme = "sphinx_rtd_theme"
+
+html_logo = "logo/logo.png"
